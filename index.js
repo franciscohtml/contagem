c6145e8d1464030a5c34f42127bd74e5b59607dd
franciscohtml <124741411+franciscohtml@users.noreply.github.com>
2023-08-31 09:14:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+tempo = 5000
+const idade = 16;
+age = 16;
+age = age + 1;
+var timer = setInterval(chamar,tempo);
+var timer = setInterval(somar,tempo);
+
+function chamar(){
+//document.getElement só é chamado com tempo
+
+document.getElementById('mostrar').innerHTML = idade;
+document.getElementById('somatoria').innerHTML = age;
+
+}
+
